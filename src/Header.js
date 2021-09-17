2af78af1896cb8434f0f47d17f308be91d1339b9
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-17 15:40:13 +0100

    Add styles to nav buttons

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.css';
+
 
 function Header() {
   return (
     <nav className={styles.header}>
       <div className={styles.leftNav}>
-        <button className="navbtn">Home</button>
-        <button className="navbtn">Boards</button>
+        <button className={styles.navbtn}>
+            <FontAwesomeIcon icon={faHome} />
+        </button>
+        <button className={styles.navbtn}>Boards</button>
         <input type="search" placeholder="Jump to..."/>
       </div>
 
@@ -15,10 +22,14 @@ function Header() {
       </div>
 
       <div className={styles.rightNav}>
-        <button className="navbtn">Create</button>
-        <button className="navbtn">Info</button>
-        <button className="navbtn">Bell</button>
-        <button className="navbtn">Profile Link</button>
+        <button className={styles.navbtn}>Create</button>
+        <button className={styles.navbtn}>
+            <FontAwesomeIcon icon={faInfoCircle}  />
+        </button>
+        <button className={styles.navbtn}>
+          <FontAwesomeIcon icon={faBell} />
+        </button>
+        <button className={styles.navbtn}>Profile Link</button>
       </div>
     </nav>
   );
