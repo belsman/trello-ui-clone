0a75a74c513540041b1f6163032c2ac3f5fcc6ce
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-19 16:40:27 +0100

    Add contents for  nav

--- src/BoardNav.js
+++ src/BoardNav.js
@@ -2,9 +2,22 @@ import React from "react";
 
 function BoardNav() {
   return (
-    <div>
-
-    </div>
+    <nav>
+      <section className="first-nav">
+        <button type="button">Board</button>
+        <h1>Alpha board</h1>
+      </section>
+      <section className="second-nav">
+        <div className="second-nav-part-1">
+          <div className="subnav-avatar"></div>
+          <button type="button">Invite</button>
+        </div>
+        <div className="second-nav-part-2">
+          <button type="button" className="automation-btn">Automation</button>
+          <button type="button" className="show-menu">Show menu</button>
+        </div>
+      </section>
+    </nav>
   );
 }
 
