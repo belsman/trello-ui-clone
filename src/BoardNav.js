5bbf6ca79f1d6249799673488e69564a41d92d4b
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-19 17:26:06 +0100

    Add styles to the button elements in the board-nav

--- src/BoardNav.js
+++ src/BoardNav.js
@@ -3,19 +3,19 @@ import styles from "./boardNav.module.css";
 
 function BoardNav() {
   return (
-    <nav>
+    <nav className={styles.subNav}>
       <section className={styles.firstNav}>
-        <button type="button">Board</button>
-        <h3 className={styles.boardName}>Alpha board</h3>
+        <button type="button" className={styles.clickableBtn}>Board</button>
+        <h3 className={`${styles.boardName} ${styles.clickableBtn}`}>Alpha board</h3>
       </section>
       <section className={styles.secondNav}>
         <div className={styles.secondNavPart1}>
-          <div className="subnav-avatar"></div>
-          <button type="button">Invite</button>
+          <div className={`${styles.navAvatar}`}></div>
+          <button type="button" className={`${styles.clickableBtn}`}>Invite</button>
         </div>
         <div className={styles.secondNavPart2}>
-          <button type="button" className="automation-btn">Automation</button>
-          <button type="button" className="show-menu">Show menu</button>
+          <button type="button" className={`${styles.clickableBtn}`} id="automation-btn">Automation</button>
+          <button type="button" id="show-menu" className={`${styles.clickableBtn}`}>Show menu</button>
         </div>
       </section>
     </nav>
