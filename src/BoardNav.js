95289fc61b043eeb30ea42c97366963ad0492a4f
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-19 17:04:03 +0100

    Add layout style to the boardNav component

--- src/BoardNav.js
+++ src/BoardNav.js
@@ -1,18 +1,19 @@
 import React from "react";
+import styles from "./boardNav.module.css";
 
 function BoardNav() {
   return (
     <nav>
-      <section className="first-nav">
+      <section className={styles.firstNav}>
         <button type="button">Board</button>
-        <h1>Alpha board</h1>
+        <h3 className={styles.boardName}>Alpha board</h3>
       </section>
-      <section className="second-nav">
-        <div className="second-nav-part-1">
+      <section className={styles.secondNav}>
+        <div className={styles.secondNavPart1}>
           <div className="subnav-avatar"></div>
           <button type="button">Invite</button>
         </div>
-        <div className="second-nav-part-2">
+        <div className={styles.secondNavPart2}>
           <button type="button" className="automation-btn">Automation</button>
           <button type="button" className="show-menu">Show menu</button>
         </div>
