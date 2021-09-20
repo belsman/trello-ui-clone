4befcb194088498fbca693722a4ffd12392a57c7
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-20 11:49:24 +0100

    Add markup for List elements

--- src/BoardDetail.js
+++ src/BoardDetail.js
@@ -8,6 +8,24 @@ function BoardDetail() {
     <div className={styles.rootDetail}>
       <Header />
       <BoardNav />
+      <main className="board">
+        <div className="board-lists">
+          <section className="list">
+            <header className="list-header">
+              <h4>To Do</h4>
+            </header>
+            <div className="cards"></div>
+            <footer className="list-footer">
+              <div className="add-card-action">
+                <button type="button">Add a card</button>
+              </div>
+            </footer>
+          </section>
+        </div>
+        <div className="create-list">
+
+        </div>
+      </main>
     </div>
   );
 }
