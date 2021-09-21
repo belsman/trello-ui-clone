0064d31b9a876ac068e7684205ed60c213a476dc
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-21 12:43:15 +0100

    Add styles to the title and button card components

--- src/Card.js
+++ src/Card.js
@@ -7,7 +7,7 @@ import styles from "./Card.module.css";
 function Card() {
   return (
     <div className={styles.card}>
-      <h4 className={styles.cardTitle}>This is my first card.</h4>
+      <span className={styles.cardTitle}>This is my first card.</span>
       <button type="button" className={styles.editTitle}>
         <FontAwesomeIcon icon={faPencilAlt} />
       </button>
