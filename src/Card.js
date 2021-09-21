dcc4e42572fb9dafe7f5f3b25d87872c35f0ea9d
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-21 12:09:52 +0100

    Add layout forthe card component

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,13 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
+import styles from "./Card.module.css";
+
 function Card() {
   return (
-    <div className="card">
-      <h4>This is my first card.</h4>
-      <button type="button">
+    <div className={styles.card}>
+      <h4 className={styles.cardTitle}>This is my first card.</h4>
+      <button type="button" className={styles.editTitle}>
         <FontAwesomeIcon icon={faPencilAlt} />
       </button>
     </div>
