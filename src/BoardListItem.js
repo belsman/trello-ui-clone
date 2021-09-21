54ff20ff19a9bad9fb9f4ca120ebb5c7aafd5bc6
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-21 11:26:24 +0100

    Add autoresizing textarea

--- src/BoardListItem.js
+++ src/BoardListItem.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+
+import TextareaAutosize from 'react-textarea-autosize';
+
 import styles from "./boardListItem.module.css";
 
 function BoardListItem() {
@@ -15,7 +18,11 @@ function BoardListItem() {
   const AddCardForm = () => {
     return (
       <div className={styles.addCardFormContainer}>
-        <textarea placeholder="Enter a title for this card..."></textarea>
+        <TextareaAutosize 
+          placeholder="Enter a title for this card..."
+          minRows={3}
+          maxRows={7}
+        />
         <div className={styles.addCardFormActions}>
           <div className={styles.buttonsGroup}>
             <button className={styles.saveCard}>Add Card</button>
