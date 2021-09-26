a1d44b3888c207366b73c914d2bc69d5566db844
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-26 12:50:49 +0100

    Componentify the board home page to render dynamic data

--- src/BoardList.js
+++ src/BoardList.js
@@ -1,43 +1,32 @@
 import React from "react";
-
 import styles from "./boardList.module.css";
 
-
 function BoardList() {
+  const boards = [ { id: 1, name: 'alpha' }, { id: 2, name: 'bravo' }, { id: 3, name: 'charlie' } ];
+
+  const BoardTile = ({ board }) => {
+    const { name } = board;
+
+    return (
+      <div className={styles.boardTile}>
+        <span>{name}</span>
+      </div>
+    );
+  };
+  
+  const renderedBoard = boards.map(item => {
+    return (
+      <li className={styles.boardItem} key={item.id}>
+        <BoardTile board={item} />
+      </li>
+    )
+  });
+
   return (
     <div className={styles.root}>
       <h3>Your boards</h3>
       <ul className={styles.boardList}>
-        <li className={styles.boardItem}>
-          <div className={styles.boardTile}>
-            <span>alpha board</span>
-          </div>
-        </li>
-        <li className={styles.boardItem}>
-          <div className={styles.boardTile}>
-            <span>bravo board</span>
-          </div>
-        </li>
-        <li className={styles.boardItem}>
-          <div className={styles.boardTile}>
-            <span>charlie board</span>
-          </div>
-        </li>
-        <li className={styles.boardItem}>
-          <div className={styles.boardTile}>
-            <span>delta board</span>
-          </div>
-        </li>
-        <li className={styles.boardItem}>
-          <div className={styles.boardTile}>
-            <span>echo board</span>
-          </div>
-        </li>
-        <li className={styles.boardItem}>
-          <div id={styles.createBoard} className={styles.boardTile}>
-            <span>Create new board</span>
-          </div>
-        </li>
+        {renderedBoard}
       </ul>
     </div>
   );
