bb1c041400a91955c112759f66100fb9e76ee1e9
Bello Babakolo <fleetbeekay@gmail.com>
2021-10-03 11:49:27 +0100

    Layout to the modal and its content

--- src/features/boards/CreateBoardButton.js
+++ src/features/boards/CreateBoardButton.js
@@ -21,14 +21,44 @@ const NewBoardForm = () => {
 };
 
 function CreateBoardButton({ styles }) {
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+  const customStyles = {
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.8)'
+    },
+    content: {
+      width: '35%',
+      height: '100px',
+      padding: '0',
+      top: '10%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      transform: 'translate(-50%, 0%)',
+      borderRadius: '5px',
+      overflow: 'hidden',
+    },
+  };
+
   return (
-    <div
-      id={styles.createBoard}
-      className={styles.boardTile}
-      onClick={() => alert("Show me! The add form modal!")}
-    >
-      <span>Create new board</span>
-    </div>
+    <>
+      <div
+        id={styles.createBoard}
+        className={styles.boardTile}
+        onClick={() => setModalIsOpen(true)}
+      >
+        <span>Create new board</span>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        style={customStyles}
+        onRequestClose={() => setModalIsOpen(false)}
+        shouldCloseOnOverlayClick={true}
+      >
+        <NewBoardForm />
+      </Modal>
+    </>
   );
 }
 
