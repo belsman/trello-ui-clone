138814039f0babce9652d5b65821e12aac925da4
Bello Babakolo <fleetbeekay@gmail.com>
2021-10-03 10:29:57 +0100

    Route to login if user is not null

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
-// import BoardDetail from "./BoardDetail";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 import BoardList from "./features/boards/BoardList";
 import Header from "./Header";
 
 function Home() {
+  const user = useSelector(state => state.user);
+
+  if (!user.id) {
+    return <Redirect to="/login" />
+  }
+
   return (
     <>
       <Header />
